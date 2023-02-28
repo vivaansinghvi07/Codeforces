17eec4de417bfa763a7b777ca7a3ea7363304aea
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-27 23:10:03 -0500

    Vanya and Fence

---
+++ problems/solved/_677A.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class _677A {
+  public static void main(String[] args) throws IOException {
+    Reader in = new Reader();
+    int cases = in.nextInt();
+    int height = in.nextInt();
+    int total = cases;
+    while (cases --> 0) {
+      if (in.nextInt() > height) total++;
+    }
+    System.out.println(total);
+  }
+}
+
+class Reader {
+  private BufferedReader br;
+  private StringTokenizer str;
+  public Reader () {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+  public String nextToken() throws IOException { 
+    while (str == null || !str.hasMoreElements()) { 
+      str = new StringTokenizer(br.readLine());
+    } 
+    return str.nextToken(); 
+  } 
+  public int nextInt() throws IOException {
+    return Integer.parseInt(this.nextToken());
+  }
+  public char[] nextCharArr() throws IOException {
+    return nextToken().toCharArray();
+  }
+  public int[] nextIntArr(int n) throws IOException {
+    int[] out = new int[n];
+    for (int i = 0; i < n; i++) {
+      out[i] = nextInt();
+    }
+    return out;
+  }
+  public int[] nextIntArr() throws IOException {
+    String[] str = br.readLine().split("\\s+");
+    int[] inp = new int[str.length];
+    for (int i = 0; i < str.length; i++) {
+      inp[i] = Integer.parseInt(str[i]);
+    }
+    return inp;
+  }
+  public long[] nextLongArr() throws IOException {
+    String[] str = br.readLine().split("\\s+");
+    long[] inp = new long[str.length];
+    for (int i = 0; i < str.length; i++) {
+      inp[i] = Long.parseLong(str[i]);
+    }
+    return inp;
+  }
+  public String nextLine() throws IOException {
+    return br.readLine();
+  }
+  public long nextLong() throws IOException {
+    return Long.parseLong(this.nextToken());
+  }
+  public double nextDouble() throws IOException {
+    return Double.parseDouble(this.nextToken());
+  }
+}
