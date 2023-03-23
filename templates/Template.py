bd7897d01223ad66ab566561825e75534e187a3e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-22 23:56:22 -0400

    added functions

--- templates/Template.py
+++ templates/Template.py
@@ -1,3 +1,3 @@
-I, IN, M, R, LN, LS, P, S, ST, A = int, input, map, range, len, list, print, sum, str, abs
+I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min
 for _ in R(I(IN())):
     code = "here"
