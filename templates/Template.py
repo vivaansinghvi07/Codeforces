1c5709ae2646e5a7513c14b16948e54e5cbfa6a8
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-01 23:26:02 -0400

    added inp function to template

--- templates/Template.py
+++ templates/Template.py
@@ -1,3 +1,3 @@
-I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min
+I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min; inp = lambda: LS(M(I, IN().split()))
 for _ in R(I(IN())):
     code = "here"
