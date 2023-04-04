55af927414e35e291e7303fd156e2c2fda813585
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-03 22:42:38 -0400

    shortened

--- problems/solved/_155A.py
+++ problems/solved/_155A.py
@@ -1,4 +1,3 @@
 I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min; inp = lambda: LS(M(I, IN().split()))
 n, a, s, mx, mn = I(IN()), inp(), 0, 0, 10000
-for l in R(n): i = a[l]; s += (i > mx or i < mn) and l != 0; mx = MX(mx, i); mn = MN(mn, i);
-P(s)
+for l in R(n): i = a[l]; s += (i > mx or i < mn) and l != 0; mx = MX(mx, i); mn = MN(mn, i); P(s) if l == n-1 else 0
