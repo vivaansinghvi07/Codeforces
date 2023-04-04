8d77ecd99cd8efdda26584c77cb1f6d9a891d2e6
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-03 22:40:51 -0400

    I_love_%username%

---
+++ Template.py
@@ -0,0 +1,4 @@
+I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min; inp = lambda: LS(M(I, IN().split()))
+n, a, s, mx, mn = I(IN()), inp(), 0, 0, 10000
+for l in R(n): i = a[l]; s += (i > mx or i < mn) and l != 0; mx = MX(mx, i); mn = MN(mn, i);
+P(s)
