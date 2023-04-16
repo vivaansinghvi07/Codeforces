b0fd21b0a18d1db8d1740ca2f1375eb420368ead
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-16 11:15:55 -0400

    Spy Detected!

---
+++ _1512A.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())): 
+    input()
+    a = list(map(int, input().split()))
+    c = {}
+    for i in a:
+        if not c[i]:
+            c[i] = 1
+        else:
+            c[i] += 1
+    for k in c:
+        if c[k] == 1:
+            print(a.index(k) + 1)
