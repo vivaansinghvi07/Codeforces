f7f69b53607bd350a68a9591d85de3aed856b9bd
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-30 22:37:51 -0400

    error fix

--- templates/Template.cpp
+++ templates/Template.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void solve() {
+    // code here
+}
+
 int main() {
     int t;
     cin >> t;
     while (t --> 0) {
         solve();
     }
-}
-
-void solve() {
-    // code here
 }
