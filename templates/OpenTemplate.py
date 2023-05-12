44942dcbae7b4a4b59b57fb5893810cb0f147637
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-05-11 23:08:59 -0400

    another template

---
+++ templates/OpenTemplate.py
@@ -0,0 +1,3 @@
+I, IN, M, R, LN, LS, P, S, ST, A, SO, MX, MN = int, input, map, range, len, list, print, sum, str, abs, sorted, max, min; inp = lambda: LS(M(I, IN().split()))
+for t in [*open("input.txt")]["""Indeces here"""]:
+    code = "here"
