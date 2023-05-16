7f6ed1e4d32fed7b755682c739889697060e2ee7
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-05-16 13:23:33 -0400

    simplified

--- problems/solved/one-liners/_1669A.py
+++ problems/solved/one-liners/_1669A.py
@@ -1,1 +1,1 @@
-print("\n".join([f"Division {5-sum([1, t >= 1400, t >= 1600, t >= 1900])}" for t in map(int, [*open(0)][1::])]))
+print("\n".join([f"Division {4-sum([t >= 1400, t >= 1600, t >= 1900])}" for t in map(int, [*open(0)][1::])]))
