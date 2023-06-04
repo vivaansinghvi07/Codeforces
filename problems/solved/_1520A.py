2edf1c2bc4ba626080c00b1660abe92376a6894e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-03 21:42:19 -0400

    shorter

--- problems/solved/_1520A.py
+++ problems/solved/_1520A.py
@@ -1,8 +1,6 @@
 for _ in range(int(input())):
-    input()
-    s = input(); temp = s[0]; uni = temp
+    input(); s = input(); temp = s[0]; uni = temp
     for c in s:
-        if c == temp:
-            continue
+        if c == temp: continue
         uni += c; temp = c
     print("YNEOS"[len(uni) != len(set(uni))::2])
