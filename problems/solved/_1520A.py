87f481bfa0036dff9de6f064af9a4268aefcc18e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-03 21:41:55 -0400

    Do Not Be Distracted!

---
+++ problems/solved/_1520A.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    input()
+    s = input(); temp = s[0]; uni = temp
+    for c in s:
+        if c == temp:
+            continue
+        uni += c; temp = c
+    print("YNEOS"[len(uni) != len(set(uni))::2])
