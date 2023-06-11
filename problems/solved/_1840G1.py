88686165bc470ef9cff246ab87f58cc741c42ded
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-11 19:14:13 -0400

    even shorter

--- problems/solved/_1840G1.py
+++ problems/solved/_1840G1.py
@@ -3,7 +3,7 @@ def s(a):
     global c, f, d
     for i in range(1000):
         n = int(input())
-        if c[n]: print(f"! {d - f.index(n)}", flush=True); exit()
+        if c[n]: exit(print(f"! {d - f.index(n)}", flush=True))
         if a == 1: f[i] = n
         c[n] = True; d += a; print(f"+ {a}", flush=True)
 s(1); s(1000)
