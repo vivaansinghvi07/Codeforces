e1f19fcdab127b70bd996d41cca2dae553cbcfdd
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-11-07 15:20:08 -0500

    made it even worse

--- problems/solved/_1895Cv2.py
+++ problems/solved/_1895Cv2.py
@@ -1,11 +1,10 @@
-n,a,o = int(input()), input().split(),0
+n,a,o,s,m,r,l=(i:=int)(input()),input().split(),0,sum,map,range,len
 e,h={1:{3:{},5:{}},2:{4:{}},3:{5:{}}},{1:{},2:{},3:{},4:{},5:{}}
 for x in a:
-    D,i=len(x),int(x)
-    for j in range(1+(D%2==0),D+1,2):
+    for j in r(1+((D:=l(x))%2==0),D+1,2):
         c=2-j//2-(3==D)-(D==2)-2*(D==1)
-        y=sum(map(int,x[c:]))-sum(map(int,x[:c]))
+        y=s(m(i,x[c:]))-s(m(i,x[:c]))
         h[j][y]=h[j].get(y,0)+1
     if D<4:e[D][D+2][y]=e[D][D+2].get(y,0)+1
     if D==1:e[D][D+4][y]=e[D][D+4].get(y,0)+1
-print(sum([h[(D:=len(x))].get(sum(map(int,x)),0)+sum([e[D-2-((-1-j)*2)][D].get(sum(map(int,x[:j]))-sum(map(int,x[j:])),0)for j in range(-1,-D//2,-1)])for x in a]))
+print(s([h[(D:=l(x))].get(s(m(i,x)),0)+s([e[D-2-((-1-j)*2)][D].get(s(m(i,x[:j]))-s(m(i,x[j:])),0)for j in r(-1,-D//2,-1)])for x in a]))
